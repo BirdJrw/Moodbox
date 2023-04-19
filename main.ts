d092592dca296a8d20c0bb85f27d205e6c903db6
BirdJrw <125318174+BirdJrw@users.noreply.github.com>
2023-04-18 21:23:28 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,23 +7,23 @@ pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
 basic.forever(function () {
     serial.writeValue("value", pins.analogReadPin(AnalogPin.P10))
     if (mood >= 0 && mood <= 204) {
-        strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
+        strip = neopixel.create(DigitalPin.P0, 22, NeoPixelMode.RGB)
         strip.showColor(neopixel.colors(NeoPixelColors.Purple))
         strip.setBrightness(255)
     } else if (mood >= 205 && mood <= 409) {
-        strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
+        strip = neopixel.create(DigitalPin.P0, 22, NeoPixelMode.RGB)
         strip.showColor(neopixel.colors(NeoPixelColors.Green))
         strip.setBrightness(255)
     } else if (mood >= 410 && mood <= 614) {
-        strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
+        strip = neopixel.create(DigitalPin.P0, 22, NeoPixelMode.RGB)
         strip.showColor(neopixel.colors(NeoPixelColors.White))
         strip.setBrightness(255)
     } else if (mood >= 615 && mood <= 819) {
-        strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
+        strip = neopixel.create(DigitalPin.P0, 22, NeoPixelMode.RGB)
         strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
         strip.setBrightness(255)
     } else if (mood >= 820 && mood <= 1023) {
-        strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
+        strip = neopixel.create(DigitalPin.P0, 22, NeoPixelMode.RGB)
         strip.showColor(neopixel.colors(NeoPixelColors.Blue))
         strip.setBrightness(255)
     }
